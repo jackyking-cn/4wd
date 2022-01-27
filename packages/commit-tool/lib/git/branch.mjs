1a32c4c4e78f6b8f4b4c34305b730a62a0766c20
jackyking-cn <jackyking.cn@gmail.com>
2022-01-27 19:30:41 +0800

    feat: pick branch type.

--- packages/commit-tool/lib/git/branch.mjs
+++ packages/commit-tool/lib/git/branch.mjs
@@ -1,36 +1,58 @@
 import dateformat from 'dateformat'
 import { execa } from 'execa'
 import prompts from 'prompts'
-import { hint, warn } from '../logger.mjs'
+import { empty, hint, prompt, warn } from '../logger.mjs'
 
-export function generateName(str) {
-    if (!str) {
+const branchType = {
+    feature: 'new feature',
+    hotfix: 'fix bug',
+}
+const choices = Object.keys(branchType).map((key) => ({
+    title: `${key} - ${branchType[key]}`,
+    value: key,
+}))
+const nameReg = /^[a-zA-Z0-9]+([-_ ][a-zA-Z0-9]+)*$/
+
+async function getValue(key, options, allowEmpty = false) {
+    const result = await prompts(options)
+
+    if (!allowEmpty && !result[key]) {
+        console.log()
+        process.exit(0)
+    }
+
+    return result[key]
+}
+
+export function generateName(type, name) {
+    if (!type || !name) {
         return ''
     }
 
-    const date = dateformat(new Date(), 'yyyymmdd')
-    const name = str.toLowerCase().split(' ').join('-')
+    const d = dateformat(new Date(), 'yyyymmdd')
+    const n = name.toLowerCase().split(' ').join('-')
 
-    return `feature\/${name}-${date}`
+    return `${type}\/${n}-${d}`
 }
 
 export async function generate() {
-    const nameReg = /^[a-zA-Z0-9]+([-_ ][a-zA-Z0-9]+)*$/
+    prompt('Generate branch', '', false)
+    const type = await getValue('type', {
+        type: 'select',
+        name: 'type',
+        message: 'Pick a branch type',
+        choices,
+    })
+    const name = await getValue('name', {
+        type: 'text',
+        name: 'name',
+        message: 'Please input branch name',
+        validate: (str) => nameReg.test(str) || 'Branch name is invalid.',
+    })
+    const branchName = generateName(type, name)
+    empty()
 
     try {
-        const { name } = await prompts({
-            type: 'text',
-            name: 'name',
-            message: 'Please input branch name',
-            validate: (str) => nameReg.test(str) || 'Branch name is invalid.',
-        })
-
-        if (!name) {
-            console.log()
-            process.exit(0)
-        }
-
-        const branchName = generateName(name)
         const { stdout } = await execa('git', ['branch', '--list'])
 
         if (stdout.includes(branchName)) {
--- packages/commit-tool/test/branch.test.mjs
+++ packages/commit-tool/test/branch.test.mjs
@@ -3,13 +3,13 @@ import dateformat from 'dateformat'
 import { generateName } from '../lib/git/branch.mjs'
 
 describe('branch name', () => {
-    it('should return feature/[name]-[yyyymmdd]', () => {
+    it('should return [type]/[name]-[yyyymmdd]', () => {
         const date = dateformat(new Date(), 'yyyymmdd')
 
-        expect(generateName('test')).to.equal(`feature/test-${date}`)
-        expect(generateName('test1_test1')).to.equal(`feature/test1_test1-${date}`)
-        expect(generateName('test2-test2')).to.equal(`feature/test2-test2-${date}`)
-        expect(generateName('test3 test3 test3')).to.equal(`feature/test3-test3-test3-${date}`)
-        expect(generateName('Test4 teSt4')).to.equal(`feature/test4-test4-${date}`)
+        expect(generateName('feature', 'test')).to.equal(`feature/test-${date}`)
+        expect(generateName('feature', 'test1_test1')).to.equal(`feature/test1_test1-${date}`)
+        expect(generateName('feature', 'test2-test2')).to.equal(`feature/test2-test2-${date}`)
+        expect(generateName('hotfix', 'test3 test3 test3')).to.equal(`hotfix/test3-test3-test3-${date}`)
+        expect(generateName('hotfix', 'Test4 teSt4')).to.equal(`hotfix/test4-test4-${date}`)
     })
 })
