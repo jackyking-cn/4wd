7ce44814915949842ffac3c488e14eafe7d9c1cb
jackyking-cn <jackyking.cn@gmail.com>
2022-01-27 20:22:59 +0800

    chore: add eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    root: true,
+    extends: 'eslint:recommended',
+    env: {
+        es6: true,
+        mocha: true,
+        node: true,
+    },
+    parserOptions: {
+        ecmaVersion: 10,
+        sourceType: 'module',
+    },
+    rules: {},
+}
